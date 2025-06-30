a28826ba08a1b2c2ab078d4ea7a5addbc1daa066
orlee <71377427+sirorlee@users.noreply.github.com>
2025-06-30 10:10:34 -0500

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client']
+  }
+}
+
+module.exports = nextConfig
